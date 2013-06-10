87835ab794c472f7b92e9a80e1ff1a60687a8a32
James Kassemi <jkassemi@gmail.com>
2013-06-10 01:23:11 -0600

    gofmt on 01e4795

--- ddocs_test.go
+++ ddocs_test.go
@@ -2,20 +2,20 @@ package couchbase
 
 import (
 	"encoding/json"
-        "strings"
+	"strings"
 	"testing"
 )
 
 func TestViewDefinitionReduceOmission(t *testing.T) {
-  view, e := json.Marshal(ViewDefinition {
-    Map: `function(doc, meta){ emit(meta.id, doc); }`,
-  })
+	view, e := json.Marshal(ViewDefinition{
+		Map: `function(doc, meta){ emit(meta.id, doc); }`,
+	})
 
-  if e != nil {
-    panic(e.Error())
-  }
+	if e != nil {
+		panic(e.Error())
+	}
 
-  if strings.Index(string(view), "reduce") != -1 {
-    t.Fatalf("Reduce should be omitted if empty")
-  }
+	if strings.Index(string(view), "reduce") != -1 {
+		t.Fatalf("Reduce should be omitted if empty")
+	}
 }
